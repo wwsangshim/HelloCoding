43a4fa95266d08e7a68b7934c09a305a49218d2e
wwsangshim <aiproject916@gmail.com>
2022-09-27 22:41:57 +0900

    chapter2 test

---
+++ projects/intro/02/chapter2_test/chapter2_test/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+Console.WriteLine("출력");
+Console.Write("문");
+Console.Write("\"");
+Console.Write("문제");
+
