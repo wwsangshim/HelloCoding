517dc50bbdc515dc03f1beec6d7c695fc7c92e4b
wwsangshim <aiproject916@gmail.com>
2022-09-27 23:05:29 +0900

    all

--- projects/intro/02/chapter2_test/chapter2_test/Program.cs
+++ projects/intro/02/chapter2_test/chapter2_test/Program.cs
@@ -1,8 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Runtime.InteropServices;
+//2-1
+Console.WriteLine("Hello C#");
+Console.Write("10+10");
+Console.WriteLine("=20");
+Console.WriteLine("C#의 세계에 오신것을 환영합니다");
+Console.WriteLine("감사합니다");
 
-Console.WriteLine("출력");
+//2-2
+Console.WriteLine("출력문");
+Console.Write("abcde");
+Console.Write("가나다라마바");
+Console.WriteLine("qwert");
+
+//2-3
+Console.Write("출력");
 Console.Write("문");
 Console.Write("\"");
+Console.Write(" 기초");
 Console.Write("문제");
 
