7360a8854182ee585a98638eea754cd51368f97a
wwsangshim <aiproject916@gmail.com>
2022-09-28 15:13:35 +0900

    chapter3_test

---
+++ projects/intro/03/chapter3_test/chapter3_test/chapter3_test/Program.cs
@@ -0,0 +1,43 @@
+﻿//3-1
+//1. 정수 2.문자 3.실수 4.문자 5.문자
+
+//3-2
+using System;
+
+string name = "홍길동";
+Console.Write("이름: ");
+Console.WriteLine(name);
+
+int age = 23;
+Console.Write("나이: ");
+Console.WriteLine(age);
+
+//3-3
+
+
+string stringOut = "문자열 출력";
+Console.WriteLine(stringOut);
+            
+double number1 = 21.31;
+Console.WriteLine(number1);
+
+string success = "출력성공";
+Console.WriteLine(success);
+
+//심화문제
+//3-1
+int number5 = 286;
+Console.WriteLine(number5);
+
+double number2 = 3.141592;
+Console.WriteLine(number2);
+
+string sentence1 = "문자열도 있음";
+Console.WriteLine(sentence1);
+
+//3-2
+double number3 = 22.7;
+Console.WriteLine(number3);
+
+int number4 = -13;
+Console.WriteLine(number4);
