1e833fc683a004c53cbad7ae0c00e272f99111ee
wwsangshim <aiproject916@gmail.com>
2022-09-28 11:10:29 +0900

    Constant

---
+++ projects/intro/03/Constant/Constant/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Constant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String appleCountDescription = "바구니안에 담긴사과의 개수:";
+            Console.Write(appleCountDescription);
+            int appleCount = 12;
+            Console.WriteLine(appleCount);
+
+            Console.WriteLine(-2);
+
+            string basketWeightDescription = "사과 바구니의 무게:";
+            Console.Write(basketWeightDescription);
+            double basketWeight = 1.32;
+            Console.WriteLine(basketWeight);
+        }
+    }
+}
