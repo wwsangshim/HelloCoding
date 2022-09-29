6af6b71a29a51d4782432349e363d07ace503ce8
wwsangshim <aiproject916@gmail.com>
2022-09-29 14:42:19 +0900

    UB05

--- projects/intro/UltimateBaseball/UltimateBaseball/UltimateBaseball/Program.cs
+++ projects/intro/UltimateBaseball/UltimateBaseball/UltimateBaseball/Program.cs
@@ -5,14 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("+---------------------------------------------------+");
-            Console.WriteLine(": 궁극의 숫자 야구 게임                             :");
+            Console.WriteLine("|       궁극의 숫자 야구 게임                       |");
             Console.WriteLine("+---------------------------------------------------+");
-            Console.WriteLine(":컴퓨터가 수비수가 되어 세자릿수를 하나 골랐습니다  :");
-            Console.WriteLine(":각 숫자는 0~9중에 하나며 중복되는 숫자는 없습니다  :");
-            Console.WriteLine(":모든 숫자와 위치를 맞히면 승리합니다               :");
-            Console.WriteLine(":숫자와 순서가 둘다 맞으면 스트라이크입니다         :");
-            Console.WriteLine(":숫자만 맞고 순서가 틀리면 볼입니다                 :");
-            Console.WriteLine(":숫자가 틀리면 아웃입니다                           :");
+            Console.WriteLine("|컴퓨터가 수비수가 되어 세자릿수를 하나 골랐습니다  |");
+            Console.WriteLine("|각 숫자는 0~9중에 하나며 중복되는 숫자는 없습니다  |");
+            Console.WriteLine("|모든 숫자와 위치를 맞히면 승리합니다               |");
+            Console.WriteLine("|숫자와 순서가 둘다 맞으면 스트라이크입니다         |");
+            Console.WriteLine("|숫자만 맞고 순서가 틀리면 볼입니다                 |");
+            Console.WriteLine("|숫자가 틀리면 아웃입니다                           |");
             Console.WriteLine("+---------------------------------------------------+");
 
             Console.WriteLine(">수비시가 고른 숫자");
@@ -30,6 +30,22 @@
             Console.WriteLine(number1); 
             Console.WriteLine(number2);
             Console.WriteLine(number3);
+
+            Console.WriteLine("> 첫번째 숫자를 입력하세요");
+            int guess1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("> 두번째 숫자를 입력하세요");
+            int guess2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("> 세번째 숫자를 입력하세요");
+            int guess3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(">공격수가 고른 숫자");
+            Console.WriteLine(guess1);
+            Console.WriteLine(guess2);
+            Console.WriteLine(guess3);
+
+
         }
     }
 }
