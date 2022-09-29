07225bd07c09263042fce9782268aaa3bb71bc5f
wwsangshim <aiproject916@gmail.com>
2022-09-29 10:28:46 +0900

    chapter04_test

---
+++ projects/intro/04/chapter04_test/chapter04_test/Program.cs
@@ -0,0 +1,61 @@
+﻿//4-1
+Console.WriteLine(10 + 5);
+Console.WriteLine(20-10);
+Console.WriteLine(4*7);
+Console.WriteLine(20/7);
+Console.WriteLine(20%7);
+
+Console.WriteLine("---------------------------------------------");
+//4-2
+Console.WriteLine("안녕"+"하세요");
+Console.WriteLine("---------------------------------------------");
+//4-3
+int number1 = 20;
+int number2 = 5;
+int number3 = 3;
+int number4 = 13;
+int number5;
+
+number1= number1+number2;
+number2= number3+number4;
+number4 = number1 - number4;
+number3 = number2 / number4;
+number5 = number2 % number4;
+
+Console.WriteLine(number1);
+Console.WriteLine(number2);
+Console.WriteLine(number3);
+Console.WriteLine(number4);
+Console.WriteLine(number5);
+// 25 16 1 12 14
+
+
+Console.WriteLine("---------------------------------------------");
+//4-4
+int number6 = 4-2;
+number6 = number6 * 16;
+number6 = number6 + 2;
+Console.WriteLine("(4-2)*16 +2");
+Console.WriteLine(number6);
+Console.WriteLine("---------------------------------------------");
+//심화 4-1
+int number = 2;
+number = number * 3;
+number = number - 4;
+number = number % 2;
+
+Console.WriteLine(number);
+Console.WriteLine("---------------------------------------------");
+//심화4-2
+int bag = 10;
+int aram;
+int uram;
+aram = (bag / 2)-1;
+bag = bag - aram;
+uram = (bag / 2)+2;
+bag = bag - uram;
+
+
+Console.WriteLine(aram);
+Console.WriteLine(uram);
+Console.WriteLine(bag);
