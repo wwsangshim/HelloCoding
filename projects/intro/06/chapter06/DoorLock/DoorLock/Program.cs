f1cb5c2957df9d4d821199755657241a0db258eb
wwsangshim <aiproject916@gmail.com>
2022-10-01 16:59:52 +0900

    doorlock

---
+++ projects/intro/06/chapter06/DoorLock/DoorLock/Program.cs
@@ -0,0 +1,15 @@
+﻿int passcodeNumber1 = 6;
+int pssscodeNumber2 = 2;
+
+Console.WriteLine("첫번째 숫자를 넣어주세요");
+int userInput1 = int.Parse(Console.ReadLine());
+Console.WriteLine("두번쨰 숫자를 넣어주세요");
+int userInput2 = int.Parse(Console.ReadLine());
+
+if (userInput1 == passcodeNumber1)
+{
+    if (userInput2 == passcodeNumber1)
+    {
+        Console.WriteLine("문이 열렸습니다");
+    }
+}
