d25ffefde2340d02f4a1652e05910e536356d712
wwsangshim <aiproject916@gmail.com>
2022-10-01 15:25:10 +0900

    bool

---
+++ projects/intro/06/chapter06/ComparisonOperators/ComparisonOperators/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("10>10은");
+Console.WriteLine(10 > 10);
+
+Console.Write("10>=10은");
+Console.WriteLine(10>=10);
+
+Console.Write("5<10은");
+Console.WriteLine("5<10");
+
+Console.Write("5<=10은");
+Console.WriteLine("5<=10");
+
+Console.Write("5==4는");
+Console.WriteLine(5 == 4);
+
+Console.Write("5!=4는");
+Console.WriteLine(5 != 4);
+
+
+
+
