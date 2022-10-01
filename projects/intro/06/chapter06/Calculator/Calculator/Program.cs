4c1c918965ce8032513b03dce58d31685ff42642
wwsangshim <aiproject916@gmail.com>
2022-10-01 15:02:17 +0900

    chapter06

---
+++ projects/intro/06/chapter06/Calculator/Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("첫번째 숫자를 입력하세요");
+string userInput1 = Console.ReadLine();
+int number1 = int.Parse(userInput1);
+Console.WriteLine("두번째 숫자를 입력하세요");
+string userInput2 = Console.ReadLine();
+int number2 = in.Parse(userInput2);
+
+Console.WriteLine("연산자를 입력하세요");
+string InputOperator =Console.ReadLine();
+
+
+Console.Write(number1);
+Console.Write(InputOperator);
+
+Console.Write(number2);
+Console.Write("=");
+Console.WriteLine(number1 inputOperator number2);
+;
