1ecdbeefaca88c94cbcc0ace4937b6b6fc41eab6
wwsangshim <aiproject916@gmail.com>
2022-10-02 17:01:39 +0900

    chapter06

--- projects/intro/UltimateBaseball/UltimateBaseball/UltimateBaseball/Program.cs
+++ projects/intro/UltimateBaseball/UltimateBaseball/UltimateBaseball/Program.cs
@@ -15,19 +15,11 @@
             Console.WriteLine("|숫자가 틀리면 아웃입니다                           |");
             Console.WriteLine("+---------------------------------------------------+");
 
-            Console.WriteLine(">수비시가 고른 숫자");
+            Console.WriteLine(">수비수가 고른 숫자");
             int number1 = 3;
             int number2 = 1;
             int number3 = 9;
             Console.WriteLine(number1);
-            Console.WriteLine(number2);
-            Console.WriteLine(number3);
-
-            Console.WriteLine("> 수비수가 다시 고른 숫자");
-            number1 = 8;
-            number2 = 2;
-            number3 = 0;
-            Console.WriteLine(number1); 
             Console.WriteLine(number2);
             Console.WriteLine(number3);
 
@@ -46,6 +38,48 @@
             Console.WriteLine(guess3);
 
 
+
+            if (guess1 == guess2 || guess1 == guess3 || guess3 == guess2)
+            {
+                Console.WriteLine("같은 숫자를 입력하면 안됩니다");
+            }
+            else
+            {
+                int strikeCount = 0;
+                int ballCount = 0;
+
+                if (guess1 == number1)
+                {
+                    strikeCount = strikeCount + 1;
+                }
+                else if (guess1 == number2 || guess2 == number3)
+                {
+                    ballCount = ballCount + 1;
+                }
+                if (guess2 == number1)
+                {
+                    strikeCount = strikeCount + 1;
+                }
+                else if (guess2 == number1 || guess2 == number3)
+                {
+                    ballCount = ballCount + 1;
+                }
+                if (guess3 == number3)
+                {
+                    strikeCount = strikeCount + 1;
+                }
+                else if (guess3 == number1 || guess3 == number2)
+                {
+                    ballCount = ballCount + 1;
+                }
+
+                Console.Write("스트라이트: ");
+                Console.WriteLine(strikeCount);
+                Console.Write("볼: ");
+                Console.WriteLine(ballCount);
+                Console.Write("아웃: ");
+                Console.WriteLine(3 - strikeCount - ballCount);
+            }
         }
     }
 }
